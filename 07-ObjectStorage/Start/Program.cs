8267be711529031a4116cbcfe593516512e336b7
i.kucherov <i.kucherov@bettube.com>
2023-03-14 15:54:03 +0930

    Complete module 7

--- 07-ObjectStorage/Start/Program.cs
+++ 07-ObjectStorage/Start/Program.cs
@@ -2,9 +2,57 @@
 
 // Create instance of view model
 SerializeViewModel vm = new();
- 
-// Call Sample Method
+
+Console.WriteLine("Serialize");
 vm.SerializeProduct();
 
-// Stop console to view results
-Console.ReadKey();
+Console.WriteLine("\n\nDeserialize");
+vm.DeserializeProduct();
+
+FormatViewModel vm2 = new();
+
+Console.WriteLine("\n\nSerialize with formatting");
+vm2.SerializeProduct();
+
+Console.WriteLine("\n\nDeserialize with formatting");
+vm2.DeserializeProduct();
+
+ExtensionViewModel vm3 = new();
+
+Console.WriteLine("\n\nSerialize via extension method");
+vm3.SerializeProduct();
+
+Console.WriteLine("\n\nDeserialize via extension method");
+vm3.DeserializeProduct();
+
+AttributesViewModel vm4 = new();
+
+Console.WriteLine("\n\nSerialize with attributes");
+vm4.SerializeProduct();
+
+Console.WriteLine("\n\nDeserialize with attributes");
+vm4.DeserializeProduct();
+
+NestedViewModel vm5 = new();
+
+Console.WriteLine("\n\nSerialize product nested with sales");
+vm5.SerializeProductSales();
+
+Console.WriteLine("\n\nDeserialize product nested with sales");
+vm5.DeserializeProductSales();
+
+DataContractViewModel vm6 = new();
+
+Console.WriteLine("\n\nSerialize product with data contract serializer");
+vm6.SerializeProduct();
+
+Console.WriteLine("\n\nDeserialize product with data contract serializer");
+vm6.DeserializeProduct();
+
+BinaryViewModel vm7 = new();
+
+Console.WriteLine("\n\nSerialize product with binary serializer");
+vm7.SerializeProduct();
+
+Console.WriteLine("\n\nDeserialize product with binary serializer");
+vm7.DeserializeProduct();
--- 07-ObjectStorage/Start/ViewModels/NestedViewModel.cs
+++ 07-ObjectStorage/Start/ViewModels/NestedViewModel.cs
@@ -1,52 +1,58 @@
-﻿namespace XMLSamples {
-  public class NestedViewModel {
-    public NestedViewModel() {
-      // TODO: MODIFY YOUR FILE LOCATION
-      XmlFileName = @"D:\Samples\ProductSales.xml";
+﻿namespace XMLSamples
+{
+    public class NestedViewModel
+    {
+        public NestedViewModel()
+        {
+            Path = "../../../../../Xml/";
+            XmlFileName = $"{Path}ProductSales.xml";
+        }
+
+        private readonly string Path;
+        private readonly string XmlFileName;
+
+        #region SerializeProductSales Method
+        /// <summary>
+        /// Serialize a nested object to XML
+        /// </summary>
+        public string SerializeProductSales()
+        {
+            ProductSales prod = ProductSalesRepository.Get();
+            string value = string.Empty;
+
+            // TODO: Serialize the object
+            value = prod.Serialize();
+
+            // TODO: Write to File
+            File.WriteAllText(XmlFileName, value);
+
+            // Display Product
+            Console.WriteLine(value);
+
+            return value;
+        }
+        #endregion
+
+        #region DeserializeProductSales Method
+        /// <summary>
+        /// Deserialize XML with nested elements back into a C# class
+        /// </summary>
+        public ProductSales DeserializeProductSales()
+        {
+            ProductSales prod = new();
+            string value;
+
+            // Read from File
+            value = File.ReadAllText(XmlFileName);
+
+            // TODO: Deserialize the object
+            prod = prod.Deserialize(value);
+
+            // Display Product
+            Console.WriteLine(prod);
+
+            return prod;
+        }
+        #endregion
     }
-
-    private readonly string XmlFileName;
-
-    #region SerializeProductSales Method
-    /// <summary>
-    /// Serialize a nested object to XML
-    /// </summary>
-    public string SerializeProductSales() {
-      ProductSales prod = ProductSalesRepository.Get();
-      string value = string.Empty;
-
-      // TODO: Serialize the object
-      
-
-      // TODO: Write to File
-      
-
-      // Display Product
-      Console.WriteLine(value);
-
-      return value;
-    }
-    #endregion
-
-    #region DeserializeProductSales Method
-    /// <summary>
-    /// Deserialize XML with nested elements back into a C# class
-    /// </summary>
-    public ProductSales DeserializeProductSales() {
-      ProductSales prod = new();
-      string value;
-
-      // Read from File
-      value = File.ReadAllText(XmlFileName);
-
-      // TODO: Deserialize the object
-      
-
-      // Display Product
-      Console.WriteLine(prod);
-
-      return prod;
-    }
-    #endregion
-  }
 }
